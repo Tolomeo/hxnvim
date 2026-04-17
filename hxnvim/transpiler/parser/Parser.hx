30990a10b98c2bc198217fe832d54c0df78204de
Diego Frattini <dgfrattini@gmail.com>
2026-04-17 21:25:00 +0100

    Parser wip

--- hxnvim/transpiler/parser/Parser.hx
+++ hxnvim/transpiler/parser/Parser.hx
@@ -1,7 +1,51 @@
 package transpiler.parser;
 
-import hxjsonast.*;
+using Lambda;
+
+import haxe.Exception;
+import haxe.ds.Option;
+import hxjsonast.Json;
+import hxjsonast.Printer;
 import transpiler.State;
+
+typedef ParsedMetadata = {name:String, ?params:Array<String>};
+typedef ParsedType = String;
+
+typedef ParsedClassProperty = {
+	name:String,
+	doc:String,
+	meta:Array<ParsedMetadata>,
+	access:Array<String>,
+	type:ParsedType,
+}
+
+typedef ParsedParam = {
+	name:String,
+	constraints:Array<ParsedType>
+}
+
+typedef ParsedClassMethodArg = {name:String, type:ParsedType, optional:Bool};
+typedef ParsedClassMethodReturn = ParsedType;
+
+typedef ParsedClassMethod = {
+	name:String,
+	doc:String,
+	meta:Array<ParsedMetadata>,
+	access:Array<String>,
+	params:Array<ParsedParam>,
+	args:Array<ParsedClassMethodArg>,
+	ret:ParsedClassMethodReturn,
+}
+
+enum ParsedClassField {
+	Property(property:ParsedClassProperty);
+	Method(method:ParsedClassMethod);
+}
+
+typedef ParsedClass = {
+	name:String,
+	fields:Array<ParsedClassField>
+}
 
 class Parser {
 	private final json:hxjsonast.Json;
@@ -11,23 +55,56 @@ class Parser {
 		this.json = hxjsonast.Parser.parse(input.spec, input.file);
 	}
 
-	public function parse() {
-		trace(this.json.pos.file);
-		switch (json.value) {
-			case JNull:
-				trace('null!');
-			case JString(string):
-				trace('string!');
-			case JBool(bool):
-				trace('boolean!');
-			case JNumber(number):
-				trace('number!');
-			case JArray(values):
-				trace('array!');
-			case JObject(fields):
-				for (field in fields) {
-					trace(field.name);
-				}
+	private function query(json:Json, selector:Array<String>):Option<Json> {
+		if (selector.length < 1) {
+			return Some(json);
 		}
+
+		final key = selector.shift();
+
+		switch (json.value) {
+			case JObject(fields):
+				switch (fields.find(f -> f.name == key)) {
+					case null:
+						return None;
+					case field:
+						return this.query(field.value, selector);
+				}
+			case _:
+				throw new Exception('Cannot query a non object json ${Printer.print(json)} at position ${json.pos.file}, ${json.pos.min}-${json.pos.max}');
+		}
+	}
+
+	public function parse() {
+		return this.parseSymbol(this.json);
+	}
+
+	private function parseSymbol(json:Json) {
+		final type = switch (this.query(this.json, ['type'])) {
+			case Some(t): t;
+			case None:
+				throw new Exception('Error retrieving symbol type in ${Printer.print(this.json)}: not found');
+		}
+
+		final kind = switch (this.query(type, ['kind'])) {
+			case Some(k): switch (k.value) {
+					case JString(s): s;
+					case _:
+						throw new Exception('Error retrieving symbol type in ${Printer.print(this.json)}: not a string');
+				};
+			case None:
+				throw new Exception('Error retrieving symbol kind in ${Printer.print(type)}: not found');
+		}
+
+		return switch (kind) {
+			case "table":
+				this.parseTable(type);
+			case _:
+				trace("NOT IMPLEMENTED");
+		}
+	}
+
+	private function parseTable(table:Json) {
+		trace("Parsing table");
 	}
 }
