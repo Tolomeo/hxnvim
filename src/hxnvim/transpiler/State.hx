eb45b66612051fb0e97f12539634052195be4b99
Diego Frattini <dgfrattini@gmail.com>
2026-05-21 18:35:45 +0100

    Cleanup

--- src/hxnvim/transpiler/State.hx
+++ src/hxnvim/transpiler/State.hx
@@ -1,25 +1,6 @@
 package hxnvim.transpiler;
 
-import hxnvim.Logger;
-import hxnvim.common.RunState;
 import hxnvim.target.Target;
-
-private final logger = Logger.suffix("Transpiler");
-
-function info(msg:String) {
-	return logger.info(msg, State.consume(state -> state.output.native));
-}
-
-function warn(msg:String) {
-	return logger.warn(msg, State.consume(state -> state.output.native));
-}
-
-function error(msg:String) {
-	return logger.error(msg, State.consume(state -> state.output.native));
-}
-
-function verbose(msg:String) {
-	return logger.verbose(msg, State.consume(state -> state.output.native));
-}
+import hxnvim.common.RunState;
 
 final State = new RunState<Target>();
