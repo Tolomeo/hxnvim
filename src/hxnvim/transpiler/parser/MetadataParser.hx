c77e6beed2f15927deb886c79e40bbf97a0816eb
Diego Frattini <dgfrattini@gmail.com>
2026-05-24 18:21:40 +0100

    Cleanup

--- src/hxnvim/transpiler/parser/MetadataParser.hx
+++ src/hxnvim/transpiler/parser/MetadataParser.hx
@@ -7,35 +7,6 @@ using hxnvim.common.ArrayTools;
 import hxnvim.common.Json;
 import hxnvim.transpiler.symbol.Symbol;
 
-class AccessParser {
-	/* public static function create(name:String, ?params:Array<String>) {
-		return switch (name) {
-			case 'native', 'private': {name: name, params: params};
-			case n: throw new Exception('Error creating meta of name "${n}": unrecognised');
-		}
-	}*/
-	private final origin:Json;
-
-	public function new(origin:Json) {
-		this.origin = origin;
-	}
-
-	public function parse() {
-		final access = new Array<SymbolAccess>();
-
-		this.origin.array().map(meta -> meta.string()).iter(meta -> switch (meta) {
-			// case "static": access.push(SymbolAccess.Static);
-			case "private": access.push(SymbolAccess.Private);
-			case "protected": access.push(SymbolAccess.Private);
-			case "package": access.push(SymbolAccess.Private);
-			// TODO: info log
-			case _:
-		});
-
-		return access;
-	}
-}
-
 final metas:Map<String, SymbolMeta> = ["method" => SymbolMeta.Method, "deprecated" => SymbolMeta.Deprecated];
 
 final access:Map<String, SymbolAccess> = [
@@ -45,29 +16,10 @@ final access:Map<String, SymbolAccess> = [
 ];
 
 class MetaParser {
-	public static function create(name:String, ?params:Array<String>) {
-		return switch (name) {
-			case 'native', 'private', 'luaDotMethod': {name: name, params: params};
-			case n: throw new Exception('Error creating meta of name "${n}": unrecognised');
-		}
-	}
-
 	private final origin:Json;
 
 	public function new(origin:Json) {
 		this.origin = origin;
-	}
-
-	public function parse() {
-		final meta = new Array<Metadata>();
-
-		this.origin.array().map(m -> m.string()).iter(m -> switch (m) {
-			case "deprecated": meta.push({name: "deprecated"});
-			// TODO: info log
-			case _:
-		});
-
-		return meta;
 	}
 
 	public function parseAccess():Array<SymbolAccess> {
