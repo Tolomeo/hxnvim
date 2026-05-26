e4c29ae1551c58099ae2aebf6ecda28fd3bc51ae
Diego Frattini <dgfrattini@gmail.com>
2026-05-26 18:12:01 +0100

    Cleanup

--- src/hxnvim/transpiler/parser/LiteralTypeParser.hx
+++ src/hxnvim/transpiler/parser/LiteralTypeParser.hx
@@ -19,14 +19,14 @@ class LiteralTypeParser {
 		this.params = params.or([]);
 	}
 
-	private function parseTableLiteralType(key:Json, value:Json) {
+	function parseTableLiteralType(key:Json, value:Json) {
 		final keyType = new LiteralTypeParser(key, this.params).parse();
 		final valueType = new LiteralTypeParser(value, this.params).parse();
 
 		return 'lua.Table<${keyType}, ${valueType}>';
 	}
 
-	private function parseLiteralTableStructure(fields:Array<Json>) {
+	function parseLiteralTableStructure(fields:Array<Json>) {
 		final entries = fields.map(field -> ({
 			name: field.select('name').string(),
 			type: new LiteralTypeParser(field.select('type'), this.params).parse()
@@ -35,97 +35,128 @@ class LiteralTypeParser {
 		return '{ ${entries.join(", ")} }';
 	}
 
+	function parseBuiltin(builtin:String) {
+		return switch (this.type.select('value').string()) {
+			case "any": "Any";
+			case "boolean": "Bool";
+			case "function": "haxe.Constraints.Function";
+			case "integer": "Int";
+			// NB: didn't find exactly lightuserdata in haxe.lua
+			case "lightuserdata": "lua.UserData";
+			case "nil": "Void";
+			case "void": "Void";
+			case "number": "Float";
+			case "string": "String";
+			case "table": "lua.Table.AnyTable";
+			case "thread": throw new Exception('Unsupported builtin type value "thread" received');
+			case "userdata": "lua.UserData";
+			case v: throw new Exception('Unexpected builtin type value "${v}" received');
+		}
+	}
+
+	function parseOptional(optional:Json) {
+		return 'Null<${new LiteralTypeParser(optional, this.params).parse()}>';
+	}
+
+	function parseUnknown() {
+		return "Dynamic";
+	}
+
+	function parseUnion(types:Array<Json>) {
+		function makeUnion(members:Array<String>):String {
+			return switch (members) {
+				case [], [_]:
+					throw new Exception('Error creating union type out of ${this.type.getValue()}');
+				case [left, right]:
+					'haxe.extern.EitherType<${left}, ${right}>';
+				case m:
+					'haxe.extern.EitherType<${m.shift()}, ${makeUnion(m)}>';
+			}
+		}
+
+		return switch (types.copy().map(t -> new LiteralTypeParser(t, this.params).parse()).unique()) {
+			case [t]:
+				t;
+			case t:
+				makeUnion(t);
+		}
+	}
+
+	function parseArray(items:Json) {
+		return 'Array<${new LiteralTypeParser(items, this.params).parse()}>';
+	}
+
+	function parseFunction(func:Json) {
+		final arguments = func.select('arguments').array().map(argument -> switch (argument.select('name').string()) {
+			case '...': '___:haxe.Rest<${new LiteralTypeParser(argument.select('type'), this.params).parse()}>';
+			case argumentName: switch (argument.select('optional').boolean()) {
+					case true: '?${argumentName}:${new LiteralTypeParser(argument.select('type'), this.params).parse()}';
+					case false: '${argumentName}:${new LiteralTypeParser(argument.select('type'), this.params).parse()}';
+				}
+		});
+
+		final return_ = switch (func.select('returns').array()) {
+			case []: "Dynamic";
+			case [r]: new LiteralTypeParser(r.select('type'), this.params).parse();
+			case returns if (returns.length <= 6): Target.toHelperReference('Multireturn<${returns.map(r -> new LiteralTypeParser(r.select("type"), this.params).parse()).join(", ")}>');
+			case _: throw new Exception('Unsupported number of return types for function ${this.type.getValue()}');
+		}
+
+		return '(${arguments.join(", ")}) -> ${return_}';
+	}
+
+	function parseTable(table:Json) {
+		final indexes = table.select('indexes').array();
+		final fields = table.select('fields').array();
+
+		return switch ({
+			fields:fields, indexes:indexes
+		}) {
+			case {fields: [], indexes: []}: 'lua.Table.AnyTable';
+			case {fields: [], indexes: [index]}: this.parseTableLiteralType(index.select('key'), index.select('value'));
+			case {fields: [], indexes: idxs}: throw new Exception('Unimplemented table with multiple indexes');
+			case {fields: flds, indexes: []}: this.parseLiteralTableStructure(flds);
+			case _: throw new Exception('Unimplemented table structure with indexes');
+		}
+	}
+
+	function parseNumericLiteral() {
+		return "Float";
+	}
+
+	function parseStringLiteral() {
+		return "String";
+	}
+
+	function parseBooleanLiteral() {
+		return "Bool";
+	}
+
+	function parseTypeReference(reference:String) {
+		return switch (reference) {
+			case paramTypeReference if (this.params.find(param -> param.name == paramTypeReference) != null): paramTypeReference;
+			case typereference: Target.toTypeReference(typereference);
+		}
+	}
+
+	function parseModuleReference(reference:String) {
+		return Target.toModuleReference(reference);
+	}
+
 	public function parse() {
 		return switch (this.type.select('kind').string()) {
-			case "builtin": switch (this.type.select('value').string()) {
-					case "any": "Any";
-					case "boolean": "Bool";
-					case "function": "haxe.Constraints.Function";
-					case "integer": "Int";
-					// NB: didn't find exactly lightuserdata in haxe.lua
-					case "lightuserdata": "lua.UserData";
-					case "nil": "Void";
-					case "void": "Void";
-					case "number": "Float";
-					case "string": "String";
-					case "table": "lua.Table.AnyTable";
-					case "thread": throw new Exception('Unsupported builtin type value "thread" received');
-					case "userdata": "lua.UserData";
-					case v: throw new Exception('Unexpected builtin type value "${v}" received');
-				}
-
-			case "unknown": "Dynamic";
-
-			case "optional": 'Null<${new LiteralTypeParser(this.type.select('type'), this.params).parse()}>';
-
-			case "union":
-				function makeUnion(members:Array<String>):String {
-					return switch (members) {
-						case [], [_]:
-							throw new Exception('Error creating union type out of ${this.type.getValue()}');
-						case [left, right]:
-							'haxe.extern.EitherType<${left}, ${right}>';
-						case m:
-							'haxe.extern.EitherType<${m.shift()}, ${makeUnion(m)}>';
-					}
-				}
-
-				switch (this.type.select('types')
-					.array()
-					.copy()
-					.map(t -> new LiteralTypeParser(t, this.params).parse())
-					.unique()) {
-					case [t]: t;
-					case t: makeUnion(t);
-				}
-
-			case "array": 'Array<${new LiteralTypeParser(this.type.select('items'), this.params).parse()}>';
-
-			case "function":
-				final arguments = this.type.select('arguments').array().map(argument -> switch (argument.select('name').string()) {
-					case '...': '___:haxe.Rest<${new LiteralTypeParser(argument.select('type'), this.params).parse()}>';
-					case argumentName: switch (argument.select('optional').boolean()) {
-							case true: '?${argumentName}:${new LiteralTypeParser(argument.select('type'), this.params).parse()}';
-							case false: '${argumentName}:${new LiteralTypeParser(argument.select('type'), this.params).parse()}';
-						}
-				});
-
-				final return_ = switch (this.type.select('returns').array()) {
-					case []: "Dynamic";
-					case [r]: new LiteralTypeParser(r.select('type'), this.params).parse();
-					case returns if (returns.length <= 6): Target.toHelperReference('Multireturn<${returns.map(r -> new LiteralTypeParser(r.select("type"), this.params).parse()).join(", ")}>');
-					case _: throw new Exception('Unsupported number of return types for function ${this.type.getValue()}');
-				}
-
-				'(${arguments.join(", ")}) -> ${return_}';
-
-			case "table":
-				final indexes = this.type.select('indexes').array();
-				final fields = this.type.select('fields').array();
-
-				return switch ({
-					fields:fields, indexes:indexes
-				}) {
-					case {fields: [], indexes: []}: 'lua.Table.AnyTable';
-					case {fields: [], indexes: [index]}: this.parseTableLiteralType(index.select('key'), index.select('value'));
-					case {fields: [], indexes: idxs}: throw new Exception('Unimplemented table with multiple indexes');
-					case {fields: flds, indexes: []}: this.parseLiteralTableStructure(flds);
-					case _: throw new Exception('Unimplemented table structure with indexes');
-				}
-
-			case "numericliteral": "Float";
-
-			case "stringliteral": "String";
-
-			case "booleanliteral": "Bool";
-
-			case "typereference": switch (this.type.select('value').string()) {
-					case paramTypeReference if (this.params.find(param -> param.name == paramTypeReference) != null): paramTypeReference;
-					case typereference: Target.toTypeReference(typereference);
-				}
-
-			case "modulereference": Target.toModuleReference(this.type.select('value').string());
-
+			case "builtin": this.parseBuiltin(this.type.select('value').string());
+			case "unknown": this.parseUnknown();
+			case "optional": this.parseOptional(this.type.select('type'));
+			case "union": this.parseUnion(this.type.select('types').array());
+			case "array": this.parseArray(this.type.select('items'));
+			case "function": this.parseFunction(this.type);
+			case "table": this.parseTable(this.type);
+			case "numericliteral": this.parseNumericLiteral();
+			case "stringliteral": this.parseStringLiteral();
+			case "booleanliteral": this.parseBooleanLiteral();
+			case "typereference": this.parseTypeReference(this.type.select('value').string());
+			case "modulereference": this.parseModuleReference(this.type.select('value').string());
 			case _:
 				throw new Exception('Unrecognised type "${this.type.getValue()}" received');
 		}
